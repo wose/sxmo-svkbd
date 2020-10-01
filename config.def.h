8176b5980cf93d88ddba02d0e57463c896bd71a7
Sebastian Woetzel <wose@zuendmasse.de>
2020-10-02 00:38:07 +0200

    Changes colors and font

--- config.def.h
+++ config.def.h
@@ -3,12 +3,12 @@ static int fontsize = 20;
 static double overlay_delay = 1.0;
 static int heightfactor = 16; //one row of keys takes up 1/x of the screen height
 static const char *fonts[] = {
-	"DejaVu Sans:bold:size=20"
+	"Hurmit Nerd Font:bold:size=20"
 };
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ffffff", "#14313d" },
-	[SchemePress] = { "#ffffff", "#000000" },
-	[SchemeHighlight] = { "#58a7c6", "#005577" },
+	[SchemeNorm] = { "#eaeaea", "#161616" },
+	[SchemePress] = { "#eaeaea", "#666666" },
+	[SchemeHighlight] = { "#161616", "#e6c547" },
 };
 
